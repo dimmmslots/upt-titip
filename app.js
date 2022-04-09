eefbe01a9c14375ee612a28c2113178e86335121
dimmmslots <vctrline2048@gmail.com>
2022-04-10 06:09:51 +0700

    fungsional halaman order done

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const exp = require('constants');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const routes = require('./routes');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+let menu = [
+  {
+    nama: "Ayam Geprek",
+    slug: "ayamgeprek",
+  },
+  {
+    nama: "Ayam Goreng",
+    slug: "ayamgoreng",
+  },
+];
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get("/", (req, res) => {
+  res.render("index", {
+    menu,
+  });
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
 });
 
 module.exports = router;
